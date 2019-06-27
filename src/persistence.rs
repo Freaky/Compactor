acb8d73b83369f65604bf5d7b860f9e8cfad5454
Thomas Hurst <tom@hur.st>
2019-06-27 20:01:58 +0100

    Replace app-dirs with directories

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,25 +1,23 @@
-use std::fs::File;
 use std::path::PathBuf;
 
-use app_dirs::*;
+use directories::ProjectDirs;
 
 use crate::filesdb::FilesDb;
 use crate::settings;
 
-const APP_INFO: AppInfo = AppInfo {
-    name: "Compactor",
-    author: "Freaky",
-};
-
-pub fn conf_dir() -> PathBuf {
-    app_root(AppDataType::UserConfig, &APP_INFO).expect("config dir")
-}
-
-pub fn cache_dir() -> PathBuf {
-    app_root(AppDataType::UserCache, &APP_INFO).expect("cache dir")
-}
-
 pub fn load() {
-    FilesDb::borrow().set_backing(cache_dir().join("incompressible.dat"));
-    settings::load(conf_dir().join("config.json"));
+    let dirs = ProjectDirs::from("", "Freaky", "Compactor").expect("dirs");
+
+    if !dirs.cache_dir().exists() {
+        let _ = std::fs::create_dir_all(dirs.cache_dir());
+    }
+
+    FilesDb::borrow().set_backing(dirs.cache_dir().join("incompressible.dat"));
+
+
+    if !dirs.config_dir().exists() {
+        let _ = std::fs::create_dir_all(dirs.config_dir());
+    }
+
+    settings::load(dirs.config_dir().join("config.json"));
 }
