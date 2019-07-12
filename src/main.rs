a955e26b32db30c5d4afaa1c068cc5aa6cfb00f8
Thomas Hurst <tom@hur.st>
2019-07-12 23:54:56 +0100

    Enable printing to console with Windows subsystem

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![windows_subsystem = "windows"]
 #![allow(non_snake_case)]
 
 mod backend;
 mod background;
 mod compact;
 mod compression;
+mod config;
 mod folder;
 mod gui;
 mod persistence;
-mod config;
+
+use winapi::um::wincon::{AttachConsole, FreeConsole, ATTACH_PARENT_PROCESS};
 
 fn main() {
+    let mut rc = 0;
+
+    // Enable console printing with Windows subsystem
+    unsafe {
+        AttachConsole(ATTACH_PARENT_PROCESS);
+    }
+
     if let Err(e) = std::panic::catch_unwind(gui::spawn_gui) {
         eprintln!("Unhandled panic: {:?}", e);
-
-        std::process::exit(1);
+        rc = 1;
     }
+
+    unsafe {
+        FreeConsole();
+    }
+
+    std::process::exit(rc);
 }
