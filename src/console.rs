eed58d0b25f39cb7501a2b5d1c08f74470176b67
Thomas Hurst <tom@hur.st>
2019-07-17 16:38:28 +0100

    Add a custom panic handler, opening a console if needed

---
+++ src/console.rs
@@ -0,0 +1,46 @@
+
+// Helper functions for handling the Windows console from a GUI context.
+//
+// Windows subsystem applications must explicitly attach to an existing console
+// before stdio works, and if not available, create their own if they wish to
+// print anything.
+//
+// These functions enable that, primarily for the purposes of displaying Rust
+// panics.
+
+use winapi::um::consoleapi::{AllocConsole};
+use winapi::um::wincon::{AttachConsole, FreeConsole, GetConsoleWindow, ATTACH_PARENT_PROCESS};
+
+/// Check if we're attached to an existing Windows console
+pub fn is_attached() -> bool {
+    unsafe {
+        !GetConsoleWindow().is_null()
+    }
+}
+
+/// Try to attach to an existing Windows console, if necessary.
+///
+/// It's normally a no-brainer to call this - it just makes println! and friends
+/// work as expected, without cluttering the screen with a console in the general
+/// case.
+pub fn attach() -> bool {
+    if is_attached() {
+        return true;
+    }
+
+    unsafe { AttachConsole(ATTACH_PARENT_PROCESS) != 0 }
+}
+
+/// Try to attach to a console, and if not, allocate ourselves a new one.
+pub fn alloc() -> bool {
+    if attach() {
+        return true;
+    }
+
+    unsafe { AllocConsole() != 0 }
+}
+
+/// Free any allocated console, if any.
+pub fn free() {
+    unsafe { FreeConsole() };
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 mod backend;
 mod background;
+mod console;
 mod compact;
 mod compression;
 mod config;
@@ -10,24 +11,53 @@ mod folder;
 mod gui;
 mod persistence;
 
-use winapi::um::wincon::{AttachConsole, FreeConsole, ATTACH_PARENT_PROCESS};
+fn setup_panic() {
+    std::panic::set_hook(Box::new(|e| {
+        if !console::alloc() {
+            // No point trying to print without a console...
+            return;
+        }
+
+        println!(r#"
+Oh dear, {app} has crashed.  Sorry :(
+
+You can report this on the website at {website}/issues
+
+Please try to include everything below the line, and give some hints as to what
+you were doing - like what folder you were running it on.
+
+#############################################################################
+
+App: {app}, Version: {ver}, Build Date: {date}, Hash: {hash}
+"#, app = env!("CARGO_PKG_NAME"), website = env!("CARGO_PKG_HOMEPAGE"),
+ver = env!("VERGEN_SEMVER"), date = env!("VERGEN_BUILD_DATE").to_string(), hash = env!("VERGEN_SHA_SHORT"));
+
+        if let Some(s) = e.payload().downcast_ref::<&'static str>() {
+             println!("panic: {}", s);
+        } else {
+             println!("panic: [mysteriously lacks a string representation]");
+        }
+
+        println!("\nHit Enter to print the rest of the debug info.");
+
+        let _ = std::io::stdin().read_line(&mut String::new());
+
+        let backtrace = backtrace::Backtrace::new();
+
+        println!("\n{:?}\n", backtrace);
+        println!("Hit Enter to continue.");
+
+        let _ = std::io::stdin().read_line(&mut String::new());
+    }));
+}
 
 fn main() {
-    let mut rc = 0;
+    setup_panic();
+    console::attach();
+    let ret = std::panic::catch_unwind(gui::spawn_gui);
+    console::free();
 
-    // Enable console printing with Windows subsystem
-    unsafe {
-        AttachConsole(ATTACH_PARENT_PROCESS);
+    if ret.is_err() {
+        std::process::exit(1);
     }
-
-    if let Err(e) = std::panic::catch_unwind(gui::spawn_gui) {
-        eprintln!("Unhandled panic: {:?}", e);
-        rc = 1;
-    }
-
-    unsafe {
-        FreeConsole();
-    }
-
-    std::process::exit(rc);
 }
